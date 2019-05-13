0fcf96350c792f9a9f19bdfde190bfab7b785d1c
gkq0216 <gkq0216@126.com>
2019-05-13 13:33:23 +0800

    集成spring-boot-starter-test测试

---
+++ src/test/java/com/gkq/agin/serviceTest/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.gkq.agin.serviceTest;
+
+import com.gkq.agin.baseTest.SpringTestCase;
+import com.gkq.agin.model.User;
+import com.gkq.agin.service.UserService;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class UserServiceTest extends SpringTestCase {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private UserService userService;
+    @Test
+    public void selectUserByIdTest(){
+        User user = userService.selectUserById(10);
+        logger.info("查找结果" + user);
+    }
+
+}
